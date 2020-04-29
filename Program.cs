1793b9c64470dccb031ca79b4a67d75f63f034f8
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2020-04-29 06:41:25 +0530

    Parallel.For code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace data_parallelism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parallel.For(0,2,Method1);
+            // Parallel.For(0, 3, delegate(int i){
+            //     Console.WriteLine($"delegate:{i}");
+            // });
+            Parallel.For(0,4, i => {
+                Console.WriteLine($"Lambda: {i}");
+            });
+            Console.WriteLine("Finished");
+        }
+
+        static void Method1(int i)
+        {
+            Console.WriteLine($"{i}");
+        }
+    }
+}
