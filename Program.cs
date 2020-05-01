6b03541317cf2770ed4cd7d8f39c22435776592f
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2020-05-01 07:42:25 +0530

    ParallelLoopState object code added

--- Program.cs
+++ Program.cs
@@ -7,13 +7,30 @@ namespace data_parallelism
     {
         static void Main(string[] args)
         {
+            Parallel.For(0, 10, new ParallelOptions{ MaxDegreeOfParallelism=3 }, (i, state) => {
+                Task.Delay(1000).GetAwaiter().GetResult();
+
+                if(state.ShouldExitCurrentIteration)
+                {
+                    Console.WriteLine($"Current:{i}, LowestBreakIteration:{state.LowestBreakIteration}, IsExceptional:{state.IsExceptional}");
+                    return;
+                }
+
+                Console.WriteLine(i);
+
+                if(i == 3)
+                {
+                    //state.Stop();
+                    throw new Exception("new exception");
+                }
+            });
             //Parallel.For(0,2,Method1);
             // Parallel.For(0, 3, delegate(int i){
             //     Console.WriteLine($"delegate:{i}");
             // });
-            Parallel.For(0,4, i => {
-                Console.WriteLine($"Lambda: {i}");
-            });
+            // Parallel.For(0,4, i => {
+            //     Console.WriteLine($"Lambda: {i}");
+            // });
             Console.WriteLine("Finished");
         }
 
